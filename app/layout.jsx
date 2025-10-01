f1095ffa754ed881e14aba6eb34c7ba7933cdbe8
Grzegorz Kiełbasa <kiebasag@gmail.com>
2025-10-01 19:18:12 +0200

    feat: navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,20 +4,20 @@ import "@/assets/styles/globals.css";
 import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
-  title: "PropertyPulse",
-  description: "Find The Perfect Rental Property",
-  keywords: "rental, property, real estate",
+	title: "PropertyPulse",
+	description: "Find The Perfect Rental Property",
+	keywords: "rental, property, real estate",
 };
 
 const MainLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Navbar />
+				<main>{children}</main>
+			</body>
+		</html>
+	);
 };
 
 export default MainLayout;
