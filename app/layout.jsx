f76ab5279bad3236fa9a2bb2e1595255dc6e027f
Grzegorz Kiełbasa <kiebasag@gmail.com>
2025-10-01 18:59:09 +0200

    init

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import "@/assets/styles/globals.css";
+import "photoswipe/dist/photoswipe.css";
+
+export const metadata = {
+  title: "PropertyPulse",
+  description: "Find The Perfect Rental Property",
+  keywords: "rental, property, real estate",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
